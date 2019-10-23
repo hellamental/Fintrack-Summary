c38278153d6f517f7c2b6352b17dc5c6e4a66cac
Mitch-work <mcedawson@gmail.com>
2019-10-23 13:33:15 +1100

    iterative spreadsheet db adder

--- Pandas_test2.py
+++ Pandas_test2.py
@@ -1,18 +1,49 @@
 import pandas as pd
 
-df = pd.read_excel (r'C:\Users\Mitchell.Dawson\Dropbox\NGER 2019\1 - Recevied Docs\10.1 Manildra flour mill\Data Request Form-NGER FY2019 - Manildra Mill rev.xlsx')
+df1 = pd.read_excel (r'C:\Users\Mitchell.Dawson\Dropbox\NGER 2019\1 - Recevied Docs\10.1 Manildra flour mill\Data Request Form-NGER FY2019 - Manildra Mill rev.xlsx')
 
 
 to_drop = ['Unnamed: 0','Unnamed: 6','Unnamed: 7','Unnamed: 8','Unnamed: 9']
-df.drop(to_drop, inplace=True, axis=1)
+df1.drop(to_drop, inplace=True, axis=1)
 
 rows_to_drop = list(range(0,33)) + list(range(133,243))
-print rows_to_drop
+#print rows_to_drop
 
-df.drop(df.index[rows_to_drop], inplace=True)
-df.reset_index(inplace=True)
+df1.drop(df1.index[rows_to_drop], inplace=True)
+df1.reset_index(inplace=True)
+df1 = df1[pd.notnull(df1['Unnamed: 3'])]
+df1 = df1[pd.notnull(df1['Unnamed: 2'])]
 #print df.head(10)
 
-print(df)
+#print(df1)
+
+total_rows = len(df1.index)
+print total_rows
+
+writer = pd.ExcelWriter(r'C:\Users\Mitchell.Dawson\Dropbox\NGERS scripts\export_dataframe.xlsx', engine='xlsxwriter')
+
+df1.to_excel (writer, sheet_name='Sheet1', header=True)
+
+#test
+df2 = pd.read_excel (r'C:\Users\Mitchell.Dawson\Dropbox\NGER 2019\1 - Recevied Docs\10.2 Narrandera Flour Mills\NFM Data Request Form-NGER 2019.xlsx')
 
 
+to_drop = ['Unnamed: 0','Unnamed: 6','Unnamed: 7','Unnamed: 8','Unnamed: 9']
+df2.drop(to_drop, inplace=True, axis=1)
+
+rows_to_drop = list(range(0,33)) + list(range(133,243))
+#print rows_to_drop
+
+df2.drop(df2.index[rows_to_drop], inplace=True)
+df2.reset_index(inplace=True)
+df2 = df2[pd.notnull(df2['Unnamed: 3'])]
+df2 = df2[pd.notnull(df2['Unnamed: 2'])]
+#print df.head(10)
+
+#print(df1)
+
+
+df2.to_excel (writer, sheet_name='Sheet1', startrow=total_rows+1, header=False)
+writer.save()
+
+#test
